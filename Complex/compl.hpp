92bbd79acc34511f6eca4dc109ddb3d34b9bf5a7
stanislav <stas00333@gmail.com>
2021-09-24 14:06:54 +0300

    created class

---
+++ Complex/compl.hpp
@@ -0,0 +1,21 @@
+#ifndef COMPLEX_HPP_INCLUDED
+#define COMPLEX_HPP_INCLUDED
+
+//=======================================================
+
+class complex
+{
+    private:
+        double Re;
+        double Im;
+
+    public:
+        complex ();
+        ~complex () {};
+
+        abs ();
+};
+
+//=======================================================
+
+#endif
