dc5532ce4e1518ca438f91d267b7493f0057fd23
stanislav <stas00333@gmail.com>
2021-10-19 18:44:42 +0300

    fix bool specialization

--- Stack/stack/stack_bool_impl.hpp
+++ Stack/stack/stack_bool_impl.hpp
@@ -1,124 +1,61 @@
-#ifndef STACK_STACK_BOOL_IMPL_HPP_INCLUDED
-#define STACK_STACK_BOOL_IMPL_HPP_INCLUDED
+#ifndef STACK_STACK_BOOL_IMPL_HPP
+#define STACK_STACK_BOOL_IMPL_HPP
 
-//----------------------------------------------------------------------------------------------
+//===========================================================================================
+
+#include <iostream>
+#include <cstring>
+#include <cassert>
+
+//===========================================================================================
 
 namespace s1ky
-{  
-
-//----------------------------------------------------------------------------------------------
-
-template <> stack<bool>::stack ():
-    capacity_(init_capacity), size_(0)
 {
-    data_ = new bool[capacity_];
-    assert (data_);
+
+template <> void stack<bool>::print () const
+{
+    for (int i = 0; i < size_; i++)
+    {
+        if (data_[i] == 1)
+        {
+            std::cout << "true" << std::endl;
+        }
+        else 
+        {
+            std::cout << "false" << std::endl;
+        }
+    }
 }
 
-template <> stack<bool>::stack (bool* data, size_t size): 
-    data_(data), size_ (size), capacity_(capacity_)
+template <> void stack<bool>::expands_capacity ()
 {
-}
+    capacity_++;
 
-template <> stack<bool>::stack (const stack& other):
-    capacity_ (other.capacity_), size_ (other.size_)
-{
-    delete[] data_;
-    new bool[capacity_];
+    bool* temp = new bool[capacity_];
+    assert (temp);
 
-    assert (data_);
-    memcpy (data_, other.data_, size_);
-}
-
-template <> stack<bool>::stack (stack&& other):
-    capacity_(other.capacity_), size_(other.size_), data_(other.data_) 
-{   
-    other.data_ = nullptr;
-}
-
-template <> stack<bool>::~stack ()
-{
-    delete[] data_;
-}
-
-//----------------------------------------------------------------------------------------------
-
-template <> stack<bool>& stack<bool>::operator= (const stack& other) 
-{
-    if (this == &other) return *this;
+    memcpy(temp, data_, size_);
 
     delete[] data_;
-    
-    size_ = other.size_;
-    capacity_ = other.capacity_;
-
-    data_ = new bool[capacity_];
-    assert (data_);
-    
-    memcpy(data_, other.data_, size_ * sizeof(bool));
-    
-    return *this;
+    data_ = temp;
 }
 
-template <> stack<bool>& stack<bool>::operator= (stack&& other)
-{
-    if (this == &other) return *this;
-
-    free (data_);
-
-    data_ = other.data_;
-    size_ = other.size_;
-    capacity_ = other.capacity_;
-    
-    other.data_ = nullptr;
-    
-    return *this;
-}
-
-template <> bool stack<bool>::operator==(const stack& other) const
-{
-    if (size_ != other.size_)
-    {
-        return false;
-    }
-    if (capacity_ != other.capacity_)
-    {
-        return false;
-    }
-    if (memcmp(data_, other.data_, size_ * sizeof (bool)))
-    {
-        return false;
-    }
-    return true;
-}
-
-template <> bool stack<bool>::operator!=(const stack& other) const
-{
-    if (size_ != other.size_)
-    {
-        return true;
-    }
-    if (capacity_ != other.capacity_)
-    {
-        return true;
-    }
-    if (memcmp(data_, other.data_, size_ * sizeof (bool)))
-    {
-        return true;
-    }
-    return false;
-}
-
-//----------------------------------------------------------------------------------------------
- 
 template <> void stack<bool>::push (const bool value)
 {
-    if (size_ == capacity_)
+    if (size_ == 8 * capacity_)
     {
         expands_capacity ();
     }
 
-    data_[size_++] = value;
+    value << (8 - size_);
+    data_[0] | value;
+
+    if (size_ % 8 == 0)
+    {
+        bytes_counter++;
+    }
+
+    size_++;
 }
 
 template <> void stack<bool>::pop ()
@@ -129,65 +66,10 @@ template <> void stack<bool>::pop ()
     }
 }
 
-template <> void stack<bool>::print () const
-{
-    for (int i = 0; i < size_; i++)
-    {
-        std::cout << data_[i] << std::endl;
-    }
-}
 
-template <> void stack<bool>::expands_capacity ()
-{
-    capacity_ = capacity_++;
-
-    bool* temp = new bool[capacity_];
-    assert (temp);
-
-    memcpy(temp, data_, size_ * sizeof(bool));
-
-    delete[] data_;
-    data_ = temp;
-}
-
-template <> bool stack<bool>::is_empty () const
-{
-    return size_ == 0;
-}
-
-template <> bool& stack<bool>::top ()
-{
-    if (is_empty())
-    {
-        std::cout << "stack is empty, cant return top" << std::endl;
-        return data_[0];
-    }
-    else
-    {
-        return data_[size_ - 1];
-    }
-}
-
-template <> const bool& stack<bool>::top () const
-{
-    if (is_empty())
-    {
-        std::cout << "stack is empty, cant return top" << std::endl;
-        return data_[0];
-    }
-    else
-    {
-        return data_[size_ - 1];
-    }
-}
-
-template <> size_t stack<bool>::size () const
-{
-    return size_;
-}
 
 }//namespace s1ky
 
-//----------------------------------------------------------------------------------------------
+//===========================================================================================
 
-#endif //STACK_STACK_BOOL_IMPL_HPP_INCLUDED
+#endif //STACK_STACK_BOOL_IMPL_HPP
