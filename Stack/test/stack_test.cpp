8d6b5afea4316e2c54bdb3041f87a2230c7d8255
stanislav <stas00333@gmail.com>
2021-11-01 15:33:10 +0300

    test for double and bool

--- Stack/test/stack_test.cpp
+++ Stack/test/stack_test.cpp
@@ -127,7 +127,7 @@ TEST(double_test, test_isneq_op)
     ASSERT_EQ(res, 0);
 }
 
-stack<double> get_stack()
+stack<double> get_double_stack()
 {
     stack<double> stk;
 
@@ -142,7 +142,7 @@ TEST(double_test, test_move_eq_op)
 
     for (int i = 0; i < 100; i++) { fir_stack.push(i); }
 
-    stack<double> sec_stk = get_stack();
+    stack<double> sec_stk = get_double_stack();
 
     ASSERT_EQ(fir_stack, sec_stk);
 }
@@ -152,167 +152,135 @@ TEST(double_test, test_move_eq_op)
 
 TEST(bool_test, test_push)
 {
-    unsigned char temp[300] = {};
-    stack<bool>   test_stk  = stack<bool>(temp, 10);
+    stack<bool> test_stk;
 
-    test_stk.print();
+    for (int i = 0; i < 1000000; i++)
+    {
+        test_stk.push (true);
+        test_stk.push (false);
+        ASSERT_EQ (false, test_stk.top ());
+    }
 }
 
-/*
-TEST (bool_test, test_pop)
+TEST(bool_test, test_pop)
 {
-    stack<double> test_stk;
+    stack<bool> test_stk;
 
-    for (int i = 1; i < 10000000; i++)
+    for (int i = 1; i < 1000000; i++)
     {
-        test_stk.push (i - 0.2 * i);
+        test_stk.push(true);
 
         if (i % 3 == 0)
         {
-            test_stk.pop ();
-            ASSERT_EQ (i - 1 - 0.2 * (i - 1), test_stk.top ());
+            test_stk.pop();
+            ASSERT_EQ(true, test_stk.top());
         }
     }
 }
 
-TEST (bool_test, test_is_empty)
+TEST(bool_test, test_is_empty)
+{
+    stack<bool> test_stk;
+
+    ASSERT_EQ(1, test_stk.is_empty());
+
+    test_stk.push(true);
+
+    ASSERT_EQ(0, test_stk.is_empty());
+}
+
+TEST(bool_test, test_size)
 {
     stack<double> test_stk;
 
-    ASSERT_EQ (1, test_stk.is_empty ());
+    for (int i = 0; i < 1000; i++) { test_stk.push(true); }
 
-    test_stk.push (77.7);
+    ASSERT_EQ(test_stk.size(), 1000);
 
-    ASSERT_EQ (0, test_stk.is_empty ());
+    for (int i = 0; i < 10000; i++) { test_stk.push(false); }
+
+    ASSERT_EQ(test_stk.size(), 11000);
 }
 
-TEST (bool_test, test_size)
-{
-    stack<double> test_stk;
-
-    for (int i = 0; i < 1000; i++)
-    {
-        test_stk.push (i);
-    }
-
-    ASSERT_EQ (test_stk.size (), 1000);
-
-    for (int i = 0; i < 10000; i++)
-    {
-        test_stk.push (i);
-    }
-
-    ASSERT_EQ (test_stk.size (), 11000);
-}
-
-TEST (bool_test, test_top)
+TEST(bool_test, test_top)
 {
     stack<double> test_stk;
 
     for (int i = 0; i < 100000; i++)
     {
-        test_stk.push (i);
-        ASSERT_EQ (i, test_stk.top ());
+        test_stk.push(true);
+        ASSERT_EQ(true, test_stk.top());
     }
-}
 
-TEST (bool_test, test_const_top)
-{
-    int number_of_iterations = 10;
-
-    double* data = new double[number_of_iterations] {};
-    assert (data);
-
-    for (int i = 0; i < number_of_iterations; i++)
+    for (int i = 0; i < 100000; i++)
     {
-        data[i] = i;
+        test_stk.push(false);
+        ASSERT_EQ(false, test_stk.top());
     }
-
-    const stack<double> test_stk(data, number_of_iterations);
-
-    ASSERT_EQ (number_of_iterations - 1, test_stk.top ());
-
-    delete[] data;
 }
 
-TEST (bool_test, test_eq_op)
+TEST(bool_test, test_eq_op)
 {
     stack<double> fir_stk;
     stack<double> sec_stk;
 
-    for (int i = 0; i < 10000; i++)
-    {
-        fir_stk.push (i);
-    }
+    for (int i = 0; i < 10000; i++) { fir_stk.push(true); }
 
     sec_stk = fir_stk;
 
-    ASSERT_EQ (fir_stk, sec_stk);
+    ASSERT_EQ(fir_stk, sec_stk);
 }
 
-TEST (bool_test, test_iseq_op)
+TEST(bool_test, test_iseq_op)
 {
     stack<double> fir_stk;
     stack<double> sec_stk;
 
-    for (int i = 0; i < 10000; i++)
-    {
-        fir_stk.push (i);
-    }
+    for (int i = 0; i < 10000; i++) { fir_stk.push(false); }
 
     bool res = (sec_stk == fir_stk);
-    ASSERT_EQ (res, 0);
+    ASSERT_EQ(res, 0);
 
     sec_stk = fir_stk;
-    res = (sec_stk == fir_stk);
-    ASSERT_EQ (res, 1);
+    res     = (sec_stk == fir_stk);
+    ASSERT_EQ(res, 1);
 }
 
-TEST (bool_test, test_isneq_op)
+TEST(bool_test, test_isneq_op)
 {
     stack<double> fir_stk;
     stack<double> sec_stk;
 
-    for (int i = 0; i < 10000; i++)
-    {
-        fir_stk.push (i);
-    }
+    for (int i = 0; i < 10000; i++) { fir_stk.push(true); }
 
     bool res = (sec_stk != fir_stk);
-    ASSERT_EQ (res, 1);
+    ASSERT_EQ(res, 1);
 
     sec_stk = fir_stk;
-    res = (sec_stk != fir_stk);
-    ASSERT_EQ (res, 0);
+    res     = (sec_stk != fir_stk);
+    ASSERT_EQ(res, 0);
 }
 
-stack<double> get_stack ()
+stack<bool> get_bool_stack()
 {
-    stack<double> stk;
+    stack<bool> stk;
 
-    for (int i = 0; i < 100; i++)
-    {
-        stk.push (i);
-    }
+    for (int i = 0; i < 100; i++) { stk.push(true); }
 
     return stk;
 }
 
-TEST (bool_test, test_move_eq_op)
+TEST(bool_test, test_move_eq_op)
 {
-    stack<double> fir_stack;
+    stack<bool> fir_stack;
 
-    for (int i = 0; i < 100; i++)
-    {
-        fir_stack.push (i);
-    }
+    for (int i = 0; i < 100; i++) { fir_stack.push(true); }
 
-    stack<double> sec_stk = get_stack ();
+    stack<bool> sec_stk = get_bool_stack();
 
-    ASSERT_EQ (fir_stack, sec_stk);
+    ASSERT_EQ(fir_stack, sec_stk);
 }
 
-*/
 
 int main(int argc, char** argv)
 {
