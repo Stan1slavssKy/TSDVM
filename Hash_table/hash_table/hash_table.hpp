54197745bdcedc559450c8b37f56c0a6f3d107d8
stanislav <stas00333@gmail.com>
2021-12-29 16:38:02 +0300

    initial commit

---
+++ Hash_table/hash_table/hash_table.hpp
@@ -0,0 +1,13 @@
+#ifndef HASH_TABLE_HASH_TABLE_HPP_INCLUDED
+#define HASH_TABLE_HASH_TABLE_HPP_INCLUDED
+
+#include <cstdlib>
+
+template<typename T>
+class Hash_table
+{
+public:
+    constexpr static const size_t default_size = 16;
+};
+
+#endif // HASH_TABLE_HASH_TABLE_HPP_INCLUDED
---
+++ Hash_table/test/hash_table_test.cpp
@@ -0,0 +1,10 @@
+#include "../hash_table/hash_table.hpp"
+
+#include <gtest/gtest.h> // NOLINT
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv); // NOLINT
+
+    return RUN_ALL_TESTS(); // NOLINT
+}
