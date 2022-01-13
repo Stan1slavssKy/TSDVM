4204c738a843ce7e91ec14ce8bd9bd9451be57cb
stanislav <stas00333@gmail.com>
2022-01-13 23:46:31 +0300

    add  hash add resize

--- Hash_table/hash_table/hash_table.hpp
+++ Hash_table/hash_table/hash_table.hpp
@@ -1,29 +1,41 @@
 #ifndef HASH_TABLE_HASH_TABLE_HPP_INCLUDED
 #define HASH_TABLE_HASH_TABLE_HPP_INCLUDED
 
+#include "../queue/queue_impl.hpp"
+
 #include <cstdlib>
 
-namespace s1ky 
-{
+using namespace s1ky::list_queue;
+
+namespace s1ky {
 template<typename key_t, typename data_t>
 class Hash_table
 {
 public:
-    constexpr static size_t default_size = 16;
-    constexpr static int    rehash_size  = 2;
+    constexpr static size_t default_size = 16; // here needs static cause class member does not exist at compile time
+    constexpr static int    resize_coeff = 2;
 
     Hash_table();
-    Hash_table(size_t size, data_t init_val);
+    Hash_table(size_t size);
     Hash_table(const Hash_table<key_t, data_t>& other);
     Hash_table(Hash_table<key_t, data_t>&& other) noexcept;
     ~Hash_table();
-    
+
     Hash_table<key_t, data_t>& operator=(const Hash_table<key_t, data_t>& other);
     Hash_table<key_t, data_t>& operator=(Hash_table<key_t, data_t>&& other) noexcept;
 
+    bool find_value(const key_t value);
+
+    void add(const key_t value);
+    void remove();
+
 private:
-    size_t size_ = 0; // size without deleted values
-    key_t* keys_ = nullptr;  
+    size_t capacity_     = 0;
+    size_t size_         = 0;
+    Queue<data_t>* keys_ = nullptr; // an array that stores a pointer to a list of values
+
+    size_t hash_(key_t key);
+    void resize_();
 };
 }; // namespace s1ky
 
--- Hash_table/hash_table/hash_table_impl.hpp
+++ Hash_table/hash_table/hash_table_impl.hpp
@@ -1,41 +1,30 @@
 #ifndef HASH_TABLE_HASH_TABLE_IMPL_HPP
 #define HASH_TABLE_HASH_TABLE_IMPL_HPP
 
-#include "hash_table.hpp"
 #include "../queue/queue_impl.hpp"
+#include "hash_table.hpp"
 
+#include <cstring>
 #include <iostream>
 #include <string>
-#include <cstring>
 
-namespace s1ky
-{
+namespace s1ky {
 template<typename key_t, typename data_t>
 Hash_table<key_t, data_t>::Hash_table() : size_(default_size)
 {
-    keys_ = new key_t[size_] {};
-    for (int i = 0; i < size_; ++i)
-    {
-        list_queue::Queue<data_t> qu;
-        keys_[i] = qu.front();
-    }
+    keys_ = new Queue<data_t>[size_] {};   
 }
 
 template<typename key_t, typename data_t>
-Hash_table<key_t, data_t>::Hash_table(size_t size, data_t init_val) : size_(size)
+Hash_table<key_t, data_t>::Hash_table(size_t size) : size_(size)
 {
-    keys_ = new key_t[size] {};
-    for (int i = 0; i < size_; ++i)
-    {
-        list_queue::Queue<data_t> qu(init_val);
-        keys_[i] = qu.front();
-    }
+    keys_ = new Queue<data_t>[size_] {};
 }
 
 template<typename key_t, typename data_t>
 Hash_table<key_t, data_t>::Hash_table(const Hash_table<key_t, data_t>& other) : size_(other.size_)
 {
-    memcpy(keys_, other.keys_, size_);
+    memcpy(keys_, other.keys_, size_ * sizeof(Queue<data_t>));
 }
 
 template<typename key_t, typename data_t>
@@ -85,7 +74,76 @@ Hash_table<key_t, data_t>& Hash_table<key_t, data_t>::operator=(Hash_table<key_t
 
 //==================================================================================================================
 
+template<typename key_t, typename data_t>
+size_t Hash_table<key_t, data_t>::hash_(key_t key)
+{
+    size_t hash_result = 0;
 
-};     // namespace s1ky
+    return hash_result;
+}
+
+template<>
+size_t Hash_table<char*, char*>::hash_(char* key)
+{
+    size_t hash_result = 0;
+    
+    int idx = 0; 
+
+    while (key[idx] != '\0')
+    {
+        hash_result = (hash_result + static_cast<size_t>(key[idx])) % capacity_;
+        ++idx;
+    }
+
+    hash_result = (hash_result * 2 + 1) % capacity_;
+
+    return hash_result;
+}
+
+template<typename key_t, typename data_t>
+void Hash_table<key_t, data_t>::resize_()
+{
+    Queue<data_t>* new_keys_buffer = new Queue<data_t>[size_] {};\
+
+    memcpy(new_keys_buffer, keys_, size_ * sizeof(Queue<data_t>));
+
+    delete[] keys_;
+
+    keys_     = new_keys_buffer;
+    capacity_ = capacity_ * resize_coeff;
+}
+
+template<typename key_t, typename data_t>
+bool Hash_table<key_t, data_t>::find_value(const key_t value)
+{
+    int idx = hash_(value);
+
+    return (keys_[idx]).find_value(value);
+}
+
+template<typename key_t, typename data_t>
+void Hash_table<key_t, data_t>::add(const key_t value)
+{
+    if (size_ == capacity_)
+
+    if (size_ < capacity_)
+    {
+        if (find_value(value) == true)
+        {
+            return;
+        }
+        else
+        {
+
+        }
+    }
+    else
+    {
+        resize_();
+
+    }
+}
+
+}; // namespace s1ky
 
 #endif // HASH_TABLE_HASH_TABLE_IMPL_HPP
