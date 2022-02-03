9bbbab8ceaae5c319c6a74fc78228f5493e9e9e7
stanislav <stas00333@gmail.com>
2022-02-03 19:07:33 +0300

    rewrite some funcs and add hashing func

--- Hash_table/hash_table/hash_table.hpp
+++ Hash_table/hash_table/hash_table.hpp
@@ -1,11 +1,10 @@
 #ifndef HASH_TABLE_HASH_TABLE_HPP_INCLUDED
 #define HASH_TABLE_HASH_TABLE_HPP_INCLUDED
 
-#include "../queue/queue_impl.hpp"
+#include "../list/list_impl.hpp"
 
 #include <cstdlib>
-
-using namespace s1ky::list_queue;
+#include <optional>
 
 namespace s1ky {
 template<typename key_t, typename data_t>
@@ -13,26 +12,30 @@ class Hash_table
 {
 public:
     constexpr static size_t default_size = 16;
-    constexpr static int    resize_coeff = 2;
 
     Hash_table();
     Hash_table(size_t capacity);
+    Hash_table(const Hash_table<key_t, data_t>& other) = delete;
     Hash_table(Hash_table<key_t, data_t>&& other) noexcept;
     ~Hash_table();
 
+    Hash_table<key_t, data_t>& operator=(const Hash_table<key_t, data_t>& other) = delete;
     Hash_table<key_t, data_t>& operator=(Hash_table<key_t, data_t>&& other) noexcept;
 
-    bool find_value(const data_t value) const;
+    void set_value(key_t key, data_t value);
+    void remove   (key_t key);
 
-    void add   (const data_t value);
-    void remove(const data_t value);
+    std::optional<data_t> get_value(key_t key) const;
+
+    unsigned int murmur_hash2(data_t key) const;
 
 private:
-    size_t         capacity_ = 0;
-    size_t         size_     = 0;
-    Queue<data_t>* keys_     = nullptr;
+    size_t capacity_ = 0;
+    size_t size_     = 0;
 
-    size_t hash_(key_t key) const;
+    List<key_t, data_t>* keys_ = nullptr;
+
+    unsigned int hash_(key_t key) const;
 };
 }; // namespace s1ky
 
--- Hash_table/hash_table/hash_table_impl.hpp
+++ Hash_table/hash_table/hash_table_impl.hpp
@@ -1,24 +1,25 @@
-#ifndef HASH_TABLE_HASH_TABLE_IMPL_HPP
-#define HASH_TABLE_HASH_TABLE_IMPL_HPP
+#ifndef HASH_TABLE_HASH_TABLE_IMPL_HPP_INCLUDED
+#define HASH_TABLE_HASH_TABLE_IMPL_HPP_INCLUDED
 
-#include "../queue/queue_impl.hpp"
+#include "../list/list_impl.hpp"
 #include "hash_table.hpp"
 
 #include <cstring>
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <optional>
 
 namespace s1ky {
 template<typename key_t, typename data_t>
 Hash_table<key_t, data_t>::Hash_table() : capacity_(default_size)
 {
-    keys_ = new Queue<data_t>[capacity_] {};
+    keys_ = new List<key_t, data_t>[capacity_] {};
 }
 
 template<typename key_t, typename data_t>
 Hash_table<key_t, data_t>::Hash_table(size_t capacity) : capacity_(capacity)
 {
-    keys_ = new Queue<data_t>[capacity_] {};
+    keys_ = new List<key_t, data_t>[capacity_] {};
 }
 
 template<typename key_t, typename data_t>
@@ -55,71 +56,100 @@ Hash_table<key_t, data_t>& Hash_table<key_t, data_t>::operator=(Hash_table<key_t
 //==================================================================================================================
 
 template<typename key_t, typename data_t>
-size_t Hash_table<key_t, data_t>::hash_(key_t key) const                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+unsigned int Hash_table<key_t, data_t>::hash_(key_t key) const                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 {
-    size_t hash_result = 0;
-
-    return hash_result;
+    return murmur_hash2(key) % capacity_;
 }
-
-/*
-template<>
-size_t Hash_table<char*, char*>::hash_(char* key)
-{
-    size_t hash_result = 0;
-
-    int idx = 0;
-
-    while (key[idx] != '\0')
-    {
-        hash_result = (hash_result + static_cast<size_t>(key[idx])) % capacity_;
-        ++idx;
-    }
-
-    hash_result = (hash_result * 2 + 1) % capacity_;
-
-    return hash_result;
-}
-*/
 
 template<typename key_t, typename data_t>
-bool Hash_table<key_t, data_t>::find_value(const data_t value) const
+unsigned int Hash_table<key_t, data_t>::murmur_hash2(data_t key) const
 {
-    int idx = hash_(value);
+    const unsigned int m    = 0x5bd1e995;
+    const unsigned int seed = 0xbc9f1d34;
+    const unsigned char* data = reinterpret_cast<const unsigned char*>(key);
+
+    unsigned int len  = strlen(data);
+    unsigned int hash = seed ^ len;
+    unsigned int k    = 0;
+
+    while (len >= 4)
+    {
+        k  = data[0];
+        k |= data[1] << 8;
+        k |= data[2] << 16;
+        k |= data[3] << 24;
+
+        k *= m;
+        k ^= k >> 24;
+        k *= m;
+
+        hash *= m;
+        hash ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    switch (len)
+    {
+        case 3:
+            hash ^= data[2] << 16;
+
+        case 2:
+            hash ^= data[1] << 8;
+
+        case 1:
+            hash ^= data[0];
+            hash *= m;
+    };
+
+    hash ^= hash >> 13;
+    hash *= m;
+    hash ^= hash >> 15;
+
+    return hash;
+}
+
+//==================================================================================================================
+
+template<typename key_t, typename data_t>
+void Hash_table<key_t, data_t>::set_value(key_t key, data_t value)
+{
+    unsigned int idx = hash_(key);    
+
+    List<key_t, data_t>* list_elem = keys_[idx].find_value(key);
+
+    if (list_elem == nullptr)
+    {
+        keys_[idx].push(key, value);
+    }
+
+    ++size_;
+}
+
+template<typename key_t, typename data_t> 
+std::optional<data_t> Hash_table<key_t, data_t>::get_value(key_t key) const
+{
+    unsigned int idx = hash_(key);    
+
+    List<key_t, data_t>* list_elem = keys_[idx].find_value(key);
+
+    if (list_elem != nullptr)
+    {
+        return list_elem->data_;;
+    }
     
-    if ((keys_[idx])->find_value(value) != nullptr)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return std::nullopt;
 }
 
 template<typename key_t, typename data_t>
-void Hash_table<key_t, data_t>::add(const data_t value)
-{
-    if (size_ > capacity_)
-    {
-        std::cout << "Error, size > capacity" << std::endl;
-        return;
-    }
-
-    if (find_value(value) == false)
-    {
-        int idx = hash_(value);
-        (keys_[idx])->push(value);
-    }
-}
-
-template<typename key_t, typename data_t>
-void Hash_table<key_t, data_t>::remove(const data_t value)
+void Hash_table<key_t, data_t>::remove(key_t key)
 {   
-    int idx = hash_(value);
-    (keys_[idx])->delete_value_node(value);
-}
+    unsigned int idx = hash_(key);
+    keys_[idx].delete_value_node(key);
 
+    --size_;
+}
 }; // namespace s1ky
 
-#endif // HASH_TABLE_HASH_TABLE_IMPL_HPP
+#endif // HASH_TABLE_HASH_TABLE_IMPL_HPP_INCLUDED
